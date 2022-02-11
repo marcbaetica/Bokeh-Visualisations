1b8b6b070bd2a828b9d5fa76a38295d6d4866cd4
Marc Baetica <marc.baetica@gmail.com>
2022-02-10 22:57:18 -0500

    Figure routing through flask.

--- flask_hosting/app.py
+++ flask_hosting/app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
-
+from bokeh.resources import INLINE
+from bokeh.embed import components
 
 app = Flask('bokeh_graph')
 
 
+# @app.route('/')
+# def main_page():
+#     return render_template('index.html')
+
+script, div = components(fig)
+
 @app.route('/')
 def main_page():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=INLINE.render_js(),
+        css_resources=INLINE.render_css(),
+    ).encode(encoding='UTF-8')
 
 
 app.run(debug=True)
