deb1a77a8112f42e00ee3a20631cebbcbc0457ef
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2024-12-13 18:07:46 -0500

    Update real_time_update.py
    Restructuring.

--- updating_plot/real_time_update.py
+++ updating_plot/real_time_update.py
@@ -6,23 +6,27 @@ from threading import Thread
 from time import sleep
 
 
-increment_amount = [i for i in range(1, 4)]
-print(increment_amount)
-
-x = [1, 2, 3]
-y = [0, 0, 0]
+Popen(f'bokeh serve --show {__file__}')
 
 doc = curdoc()
-source = ColumnDataSource(data=dict(x=x, y=y))
+
+
+y = [0, 0, 0]
+x = list(range(1, len(y)+1))
+data_source = ColumnDataSource(data=dict(x=x, y=y))
+
+p = figure()
+p.vbar(x='x', top='y', source=data_source, width=0.8)
+doc.add_root(p)
 
 
 def non_blocking_new_data():
     while all([item < 50 for item in y]):
         index = choice([0, 1, 2])
-        y[index] = y[index] + choice(increment_amount)
+        y[index] = y[index] + choice(list(range(1, 4)))
         # This gets called only when there has been some change on the data source. Its purpose is to update the table.
         doc.add_next_tick_callback(callback=partial(update_plot, x, y))
-        print(source.data)
+        print(data_source.data)
         sleep(0.25)
         print([item < 50 for item in y])
 
@@ -30,13 +34,8 @@ def non_blocking_new_data():
 # from tornado import gen
 # @gen.coroutine
 def update_plot(x, y):
-    source.stream(new_data=dict(x=x, y=y), rollover=3)
+    data_source.stream(new_data=dict(x=x, y=y), rollover=3)
     # TODO: Convert to patch instead -> rollover doesn't make sense in this example's context.
-
-
-p = figure()
-p.vbar(x='x', top='y', source=source, width=0.8)
-doc.add_root(p)
 
 
 thread = Thread(target=non_blocking_new_data)
